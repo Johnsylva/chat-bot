0644bb8bcacc1e80df3d2f88d357cf266d896f71
Johnsylva <90647997+Johnsylva@users.noreply.github.com>
2026-01-14 20:24:30 -0600

    start implementing email agent - allow for email sending wiht double bracket syntax

--- agent/agent_00.py
+++ agent/agent_00.py
@@ -10,6 +10,6 @@ llm = OpenAI()
 #configuration to setup yagmail to send an email
 def send_email(body):
     yag = yagmail.SMTP(os.getenv("GMAIL_ACCOUNT"), os.getenv("GMAIL_APP_PASSWORD"))
-    yag.send(to="sylva.uwagboe@gmail.com", subject="TEST EMAIL", contents=body)
+    yag.send(to="silvaworld@yahoo.com", subject="TEST EMAIL", contents=body)
 
 send_email('Testing 1, 2, 3')
